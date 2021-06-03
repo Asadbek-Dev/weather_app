4e1dbb1c22fb839baa46532976e47c68058d5800
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-06-03 22:31:34 +0500

    bug fixed

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-let weather={
-    "apiKey":'abf3a7e75d2754970f28f7d5f57968b7',
-    fetchWeather:function(){
-        fetch("http://api.openweathermap.org/data/2.5/weather?q=tashkent&units=metric&appid=abf3a7e75d2754970f28f7d5f57968b7")
-        .then((response)=>response.json())
-        .then((data)=>console.log(data))
-    }
-}
+let weather = {
+  apiKey: "abf3a7e75d2754970f28f7d5f57968b7",
+  fetchWeather: function (city) {
+    fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector('.description').innerText=description;
+    document.querySelector('.temp').innerText=temp+"℃";
+    document.querySelector('.humidity').innerText="Humidity: "+humidity+"%";
+    document.querySelector('.wind').innerText="Wind speed: "+speed+"km/h";
+  },
+};
 weather.fetchWeather();
